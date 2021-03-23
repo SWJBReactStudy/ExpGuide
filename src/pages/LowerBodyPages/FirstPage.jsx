d68bf5ede82904f6dfeb0798c89aa22208fd894d
ArKc1009 <arkc1009@gmail.com>
2021-03-23 21:42:50 +0900

    chore: import Label

--- src/pages/LowerBodyPages/FirstPage.jsx
+++ src/pages/LowerBodyPages/FirstPage.jsx
@@ -6,6 +6,7 @@ import Colors from '../../style/KindColors';
 import Blank from '../../atomic/Blank';
 import BorderAnimate from '../../animations/BorderAnimate';
 import RangeContext from '../../contexts/RangeContext';
+import Label from '../../atomic/Label';
 
 const Container = styled.div`
   width: 600px;
@@ -26,13 +27,6 @@ const Form = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-`;
-
-const Label = styled.label`
-  font-family: 'Bebas Neue', cursive;
-  color: ${(props) => props.color};
-  text-shadow: 1px 1px 2px gray;
-  font-size: 32px;
 `;
 
 const Confirm = styled(Link)`
