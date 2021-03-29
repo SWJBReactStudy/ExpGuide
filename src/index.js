71593dbcecd549fa4cc497c1f97770406c5e9bca
Hardtack <rjsdn827@gmail.com>
2021-03-29 17:33:20 +0900

    feat: wholeBody 라우팅

--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ import RangeProvider from './provider/RangeProvider';
 import MainPage from './pages/LowerBodyPages/MainPage';
 import FirstPage from './pages/LowerBodyPages/FirstPage';
 import App from './pages/App';
+import wholeBodyPage from './components/WholeBody/wholeBodyPage';  //나중에 pages 폴더로 옯기기
 
 ReactDOM.render(
   <React.StrictMode>
     <RangeProvider>
       <BrowserRouter>
-        <Route exact path='/ExpGuide' component={App} />
+        <Route exact path='/ExpGuide' component={App} /> {/*App is select page*/}
+
+        <Route exact path='/ExpGuide/WholeBody' component={wholeBodyPage} />
         <Route exact path="/ExpGuide/BRP" component={FirstPage} />
         <Route path="/ExpGuide/BRP/main" component={MainPage} />
       </BrowserRouter>
-    </RangeProvider>
+    </RangeProvider> 
   </React.StrictMode>,
   document.getElementById('root'),
 );
